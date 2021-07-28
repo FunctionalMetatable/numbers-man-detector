b9c4499fda728829d552867c1bec2f847f8069f9
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-07-28 11:22:46 +0100

    Consistentness in js

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,5 @@
 let usernames = ['46009361']
 
-module.exports = function(username) {
-  usernames.indexOf(username) !== -1 ? throw new Error("Numbers guy detected") : ""
-  return true
+function checkUsername(username) {
+  return usernames.indexOf(username) !== -1
 }
