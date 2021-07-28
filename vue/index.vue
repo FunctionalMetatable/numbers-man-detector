daf0235f2247452b442cb19ea05542caecba3b82
FunctionalMetatable <67440879+FunctionalMetatable@users.noreply.github.com>
2021-07-28 11:32:19 +0100

    Vue

---
+++ vue/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="main">
+    <input type="text" v-model="username" />
+    <span>{{ username == '46009361' }}</span>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return { username: '' }
+    }
+  }
+</script>
