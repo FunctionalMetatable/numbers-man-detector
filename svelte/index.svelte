d810f9a989daa61e8ecb74e9484222efbb1226cd
Metaify <67440879+FunctionalMetatable@users.noreply.github.com>
2021-12-06 19:00:33 +0000

    Svelte

---
+++ svelte/index.svelte
@@ -0,0 +1,9 @@
+<script>
+  let username = "griffpatch";
+  let isDanger = false;
+  $: isDanger = username === "46009361"
+</script>
+<div class="main">
+  <input type="text" bind:value={username} />
+  <span>{isDanger}</span>
+</div>
