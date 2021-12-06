5b7d48fe86fdc09b195540225afc3f8ee23e4354
ninjamar <70116773+ninjamar@users.noreply.github.com>
2021-12-07 02:56:10 +0800

    added cpp
    See #13.

---
+++ cpp/main.cpp
@@ -0,0 +1,9 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+
+std::vector<std::string> usernames = {std::string("46009361")};
+
+bool check_usernames(std::string username){
+  return std::find(vec.begin(), vec.end(), item) == vec.end()
+}
